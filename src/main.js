d0b0de62f574b7eb52d8c2086db789296e7fd94d
imronpuji <imronpuji5@gmail.com>
2020-11-22 16:40:55 +0700

    'resubmit'

--- src/main.js
+++ src/main.js
@@ -9,7 +9,14 @@ require('firebase/firestore');
 
 
 const firebaseConfig = {
- 
+  apiKey: "AIzaSyBXH7J69ze3HQSgvoYGWN8hZL1RjA7GpHc",
+  authDomain: "getbar-26a78.firebaseapp.com",
+  databaseURL: "https://getbar-26a78.firebaseio.com",
+  projectId: "getbar-26a78",
+  storageBucket: "getbar-26a78.appspot.com",
+  messagingSenderId: "644089455457",
+  appId: "1:644089455457:web:72e4763cf2dd32ab9a5b20",
+  measurementId: "G-7BTDDZGHBL"
 };
 
 firebase.initializeApp(firebaseConfig)
